444bb6d835838891a74bd4ad889ebc0c8f2e2e9c
Arnaud <bandonkeyea@gmail.com>
2022-08-16 13:24:48 -0600

    Inherite in Item class

--- classes/book.rb
+++ classes/book.rb
@@ -1,8 +1,19 @@
-class Book
-  attr_accessor :publisher, :cover_state
+require 'date'
+require_relative './item'
 
-  def initialize(publisher, cover_state)
+class Book < Item
+  attr_accessor :publisher, :cover_state
+  attr_reader :publish_date
+
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  private
+
+  def can_be_archived?
+    @publish_date.to_date.year < Date.today.year - 10 || @cover_state == 'bad'
   end
 end
