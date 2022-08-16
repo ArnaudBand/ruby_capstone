ac443d3fbfa7e8d4d67d86255f3f03b6db7cff79
Arnaud <bandonkeyea@gmail.com>
2022-08-16 13:54:08 -0600

    require app, and create menu to choose options

--- main.rb
+++ main.rb
@@ -6,4 +6,17 @@ class Main
   def initialize
     @app = App.new
   end
+
+  def menu
+    loop do
+      option = gets.chomp.to_i
+      case option
+      when 1 then @app.list_all_books
+      when 2 then @app.list_all_labels
+      when 3 then @app.add_book
+      when 4 then exit
+      else puts 'Invalid option'
+      end
+    end
+  end
 end
