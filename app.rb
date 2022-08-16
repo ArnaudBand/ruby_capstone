ae8231ee0516c2f03c96286da43c2b9ce22acec1
Arnaud <bandonkeyea@gmail.com>
2022-08-16 13:44:32 -0600

    Use menu_option method for options

--- app.rb
+++ app.rb
@@ -6,4 +6,13 @@ class App
     @books = []
     @labels = []
   end
+
+  def menu_option
+    'Do you want to choose an option in the menu? Please type an option number:
+    1. List books
+    2. List labels
+    3. Create book
+    4. Exit
+    Please choose an option:'
+  end
 end
