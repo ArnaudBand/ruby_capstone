b8fe2b762f4350066c49181b059bb9c10cd5e178
Arnaud <bandonkeyea@gmail.com>
2022-08-16 13:37:24 -0600

    Use add_item method to push label in Item

--- classes/label.rb
+++ classes/label.rb
@@ -1,10 +1,16 @@
 class Label
-  attr_reader :id
+  attr_reader :id, :items
   attr_accessor :color, :title
 
   def initialize(color, title, id = Random.rand(1..100))
     @id = id
     @color = color
     @title = title
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.label = self unless item.class != string
   end
 end
