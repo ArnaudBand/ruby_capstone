32a2ec7540ca76cb82f1560a12d64c5792124d6e
Arnaud <bandonkeyea@gmail.com>
2022-08-17 10:18:48 -0600

    Serialize Book object with its class name and arguments

--- classes/book.rb
+++ classes/book.rb
@@ -3,12 +3,19 @@ require_relative './item'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
-  attr_reader :publish_date
+  attr_reader :publish_date, :id
 
-  def initialize(publish_date, publisher, cover_state)
-    super(publish_date)
+  def initialize(publish_date, publisher, cover_state, id = Random.rand(1..1000))
+    super(publish_date, id)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'data' => [publish_date, publisher, cover_state, id]
+    }.to_json(*args)
   end
 
   private
