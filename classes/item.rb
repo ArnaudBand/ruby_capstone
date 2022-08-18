c54c57a7207f536f3a1eeaaa983274092b5c6451
Arnaud <bandonkeyea@gmail.com>
2022-08-18 12:47:00 -0600

    Add_label method to receive the data from label

--- classes/item.rb
+++ classes/item.rb
@@ -1,18 +1,22 @@
 class Item
-  attr_accessor :publish_date, :archive
+  attr_accessor :publish_date, :archive, :label
   attr_reader :id
 
   def initialize(publish_date, id = Random.rand(1..1000))
+    @id = id
     @publish_date = publish_date
     @archive = false
-    @id = id
   end
 
   def move_to_archive
     @archive = true if can_be_archive?
   end
 
+  def add_label
+    @label = label
+  end
+
   def can_be_archive?
-    @publish_date < Date.today.year - 10
+    (Date.today << 120 <=> @publish_date).positive?
   end
 end
